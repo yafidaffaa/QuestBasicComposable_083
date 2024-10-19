9c3de6210f438b6131870192aeece830791cbe0b
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-10-19 10:46:27 +0700

    menambahkan gambar dengan deskripsi null

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -5,15 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -87,6 +90,13 @@ fun BasicCompose(modifier: Modifier = Modifier) {
             style = TextStyle(
                 fontWeight = FontWeight.Bold,
                 fontSize = 25.sp
+            )
+        )
+        Image(
+            painter = painterResource(id = R.drawable.yafi),
+            contentDescription = null,
+            modifier = Modifier.clip(
+                RoundedCornerShape(10.dp)
             )
         )
     }
